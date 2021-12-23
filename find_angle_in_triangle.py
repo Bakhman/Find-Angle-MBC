d4137ded0ebb76384e525a599ce41f395901dbeb
Bakhman <begaev7@gmail.com>
2021-12-23 21:22:42 +0300

    Update find_angle_in_triangle.py

--- find_angle_in_triangle.py
+++ find_angle_in_triangle.py
@@ -8,9 +8,5 @@ c = (a**2+b**2-2*a*b*cos(radians(A)))**.5
 c2 = c/2
 B = degrees(acos((c**2+b**2-a**2)/(2*c*b)))
 m = (c2**2+b**2-2*c2*b*cos(radians(B)))**.5
-M = str(degrees(acos((m**2+b**2-c2**2)/(2*m*b))))
-if int(re.search(r'\.[0-9]',M).group()[1])>=5:
-    M=ceil(float(M))
-else:
-    M = floor(float(M))
+M = round(degrees(acos((m**2+b**2-c2**2)/(2*m*b))))
 print(str(M)+chr(176))
